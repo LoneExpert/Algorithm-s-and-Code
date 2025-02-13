0128b60d0a3dfe4266996db464074160e508de3c
LoneExpert <aryanshubbu171@gmail.com>
2025-02-13 07:48:35 +0530

    LC 3174 Method 2

--- Vision Horizon/String/Clear_Digits.cpp
+++ Vision Horizon/String/Clear_Digits.cpp
@@ -28,3 +28,30 @@
 
 //     return 0;
 // }
+
+// Method 2
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+
+    int k = 0;
+
+    for(char c:s){
+        if(isdigit(c)){
+            if(k>0){ // using like a stack operation think k as a top of a stack
+                s[k--];
+            }
+        }
+        else{
+            s[k++] = c; // moving forward and also storing the valid character
+        }
+    }
+
+    cout<<s.substr(0,k)<<endl;
+
+    return 0;
+}
