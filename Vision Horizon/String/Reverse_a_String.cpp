022df7852832bdbc0988ace2355406fcd74b4736
LoneExpert <aryanshubbu171@gmail.com>
2025-02-13 06:23:27 +0530

    Add some notes

--- Vision Horizon/String/Reverse_a_String.cpp
+++ Vision Horizon/String/Reverse_a_String.cpp
@@ -1,4 +1,26 @@
-// Not work for space between them
+// // Not work for space between them
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     string s;
+//     cin>>s;
+//     int n = s.length();
+    
+//     for(int i=0;i<n/2;i++){
+//         // we are swapping the first half numbers to the second half of numbers which reduce the number of interration 
+//         char temp = s[i];
+//         s[i] = s[n-1-i];
+//         s[n-1-i] = temp;
+//     }   
+
+//     cout<<s<<endl;  // printing the reversed string
+
+//     return 0;
+// }
+
+
 #include<iostream>
 using namespace std;
 
@@ -7,15 +29,12 @@ int main()
     string s;
     cin>>s;
     int n = s.length();
-    
     for(int i=0;i<n/2;i++){
-        // we are swapping the first half numbers to the second half of numbers which reduce the number of interration 
-        char temp = s[i];
-        s[i] = s[n-1-i];
-        s[n-1-i] = temp;
-    }   
-
-    cout<<s<<endl;  // printing the reversed string
+        char temp = s[n-i-1];
+        s[n-i-1] = s[i];
+        s[i] = temp;
+    }
+    cout<<s<<endl;
 
     return 0;
 }
